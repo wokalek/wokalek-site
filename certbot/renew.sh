8a68ca97c6d773693500aaec733f89fc761753f5
Alexandr Wokalek <wokalek@wokalek.ru>
2023-07-23 01:24:54 +0300

    fix: Исправил renew скрипт для обновления сертификата, поменял образ nginx на образ с поддержкой quic/http3

--- certbot/renew.sh
+++ certbot/renew.sh
@@ -3,5 +3,5 @@
 # Пример:
 # 0 5 */7 * * sh /root/projects/wokalek-site/certbot/renew.sh
 
-docker compose -f ../docker-compose.prod.yml run --rm certbot renew
+docker compose -f ../docker-compose.prod.yml up -d --build certbot
 docker exec wokalek-nginx nginx -s reload
