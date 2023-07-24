9cf0e8316389ee134dfb13e68bf1b2c96f17a1ea
Alexandr Wokalek <wokalek@wokalek.ru>
2023-07-24 03:04:55 +0300

    refactor: Убрал -d флаг при скрипте renew

--- certbot/renew.sh
+++ certbot/renew.sh
@@ -3,5 +3,5 @@
 # Пример:
 # 0 5 */1 * * sh /root/projects/wokalek-site/certbot/renew.sh
 
-docker compose -f ../docker-compose.prod.yml up -d --build certbot
+docker compose -f ../docker-compose.prod.yml up --build certbot
 docker exec wokalek-nginx nginx -s reload
