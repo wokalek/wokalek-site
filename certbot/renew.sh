2bea39b4679c78c882024c7ef315e2802e99c22f
Alexandr Wokalek <wokalek@wokalek.ru>
2023-07-24 05:10:21 +0300

    fix: обновил команду для crontab renew

--- certbot/renew.sh
+++ certbot/renew.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Пример:
-# 0 5 */1 * * sh /root/projects/wokalek-site/certbot/renew.sh
+# Пример crontab:
+# 0 5 */1 * * sh /root/projects/wokalek-site/certbot/renew.sh /root/projects/wokalek-site/docker-compose.prod.yml
 
-docker compose -f ../docker-compose.prod.yml up --build certbot
+docker compose -f $1 up --build certbot
 docker exec wokalek-nginx nginx -s reload
