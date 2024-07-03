8fa83b1a591134a1e88f46db033d355aaa3a0ea1
Alexandr Wokalek <wokalek@wokalek.ru>
2024-07-04 00:22:25 +0300

    feat(django): Сделал сущность секции для галереи и привязал к сущности Изображение. Также сделал поле image_path для неё же (:

--- django/gallery/admin.py
+++ django/gallery/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 
-from .models import GalleryImage
+from .models import GalleryImage, GallerySection
 
 
 class GalleryImageAdmin(admin.ModelAdmin):
-    fields = ('create_date', 'image')
+    fields = ('create_date', 'image_path', 'section', 'image')
+    readonly_fields = ('image_path',)
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return self.readonly_fields + ('create_date',)
+
+        return self.readonly_fields
+
+
+class GallerySectionAdmin(admin.ModelAdmin):
+    fields = ('create_date', 'name')
     readonly_fields = ()
 
     def get_readonly_fields(self, request, obj=None):
@@ -15,3 +26,4 @@ class GalleryImageAdmin(admin.ModelAdmin):
 
 
 admin.site.register(GalleryImage, GalleryImageAdmin)
+admin.site.register(GallerySection, GallerySectionAdmin)
--- django/gallery/models.py
+++ django/gallery/models.py
@@ -1,10 +1,29 @@
 from django.utils import timezone
 from django.db import models
+from django.conf import settings
 
 from imagefield.fields import ImageField
 
 
 class GalleryImage(models.Model):
     create_date = models.DateTimeField('Дата создания', default=timezone.now)
-    image = ImageField('Изображение', upload_to='gallery',
+    image_path = models.URLField('Полный путь до изображения', blank=True,
+                                 null=True)
+    section = models.ForeignKey('GallerySection', on_delete=models.CASCADE,
+                                null=True, verbose_name='Раздел')
+    image = ImageField('Изображение', upload_to='gallery/image',
                        auto_add_fields=True)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        self.image_path = settings.MEDIA_URL + self.image.name
+        super().save(update_fields=['image_path'])
+
+
+class GallerySection(models.Model):
+    create_date = models.DateTimeField('Дата создания', default=timezone.now)
+    name = models.CharField('Название', max_length=50)
+
+    def __str__(self):
+        return f'[{self.id}] {self.name}'
