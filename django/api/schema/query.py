a98742522148bacb52a9ed097d758dd92b59b5f6
Alexandr Wokalek <wokalek@wokalek.ru>
2024-07-09 22:01:37 +0300

    feat(django): поменял Query в GQL, чтобы теперь доставались посты исключительно активные для публикации

--- django/api/schema/query.py
+++ django/api/schema/query.py
@@ -2,8 +2,16 @@ import strawberry
 import strawberry_django
 
 from api.types.post import PostType, PostOrder
+from blog.models import Post
 
 
 @strawberry.type
 class Query:
-    posts: list[PostType] = strawberry_django.field(order=PostOrder)
+    @strawberry_django.field
+    def posts(self, order: PostOrder | None = strawberry.UNSET) -> list[PostType]:
+        qs = Post.objects.filter(is_active=True).order_by('-pub_date')
+
+        if order is not strawberry.UNSET:
+            qs = strawberry_django.ordering.apply(order, qs)
+
+        return qs
