7cc11bbcc6547aad17817b9dca21435db4adbd71
Alexandr Wokalek <wokalek@wokalek.ru>
2024-08-10 19:18:06 +0300

    refactor: избавился от /api/drawings и /api/photos.ts

--- nuxt/src/server/api/drawings.ts
+++
@@ -1,5 +0,0 @@
-import { fetchGallery } from '@/server/utils/galleryFolderFetcher'
-
-export default defineCachedEventHandler(async () => {
-  return await fetchGallery('static/drawings')
-}, { maxAge: 2592000 })
--- nuxt/src/server/api/photos.ts
+++
@@ -1,5 +0,0 @@
-import { fetchGallery } from '@/server/utils/galleryFolderFetcher'
-
-export default defineCachedEventHandler(async () => {
-  return await fetchGallery('static/photos')
-}, { maxAge: 2592000 })
