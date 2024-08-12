dbbaad0a223383c1e2b3743bb11a7e42f537cccc
Alexandr Wokalek <wokalek@wokalek.ru>
2024-08-12 16:39:00 +0300

    refactor: app/settings/base.py

--- django/app/settings/base.py
+++ django/app/settings/base.py
@@ -100,10 +100,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [BASE_DIR / 'staticfiles']
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 
