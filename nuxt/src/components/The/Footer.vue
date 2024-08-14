fc4649f5012d499df377748007fcbb1635c72fbb
Alexandr Wokalek <wokalek@wokalek.ru>
2024-08-14 17:55:02 +0300

    feat(nuxt): отказался от GeneralNav в футере

--- nuxt/src/components/The/Footer.vue
+++ nuxt/src/components/The/Footer.vue
@@ -1,8 +1,12 @@
 <template>
   <footer class="py-32-16 mb-16-16 relative flex justify-center">
-    <div class="caption px-16-16 absolute flex justify-between items-center w-full h-1/2 box-border left-0 bottom-0">
-      <GeneralNav class="text-gray-6 dark:text-gray-2" :links="[{ to: statisticUrl, text: 'Статистика' }]" />
-      <GeneralNav class="text-gray-6 dark:text-gray-2" :links="[{ to: { name: 'site' }, text: 'О сайте' }]" />
+    <div class="caption px-16-16 absolute flex justify-between items-center w-full h-1/2 box-border left-0 bottom-0 text-gray-6 dark:text-gray-2">
+      <GeneralLink :to="statisticUrl">
+        Статистика
+      </GeneralLink>
+      <GeneralLink :to="{ name: 'site' }">
+        О сайте
+      </GeneralLink>
     </div>
     <SvgoLogoSign class="relative w-auto fluid-[height,20-16] text-gray-3 dark:text-gray-4" :font-controlled="false" />
   </footer>
