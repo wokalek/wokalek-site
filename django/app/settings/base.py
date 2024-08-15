f399e9163b3e95dab3cea4d841718e9f35299870
Alexandr Wokalek <wokalek@wokalek.ru>
2024-08-15 19:35:18 +0300

    feat: добавил использование postgres в качестве основной БД для Django, ресетнул все миграции и создал заново

--- django/app/settings/base.py
+++ django/app/settings/base.py
@@ -75,10 +75,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db_url('DATABASE_URL')
 }
 
 # Password validation
