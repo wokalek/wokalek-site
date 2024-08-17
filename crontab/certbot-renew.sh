9595e1e79b77b0121f1e7c159cfc9eae3ae90b71
Alexandr Wokalek <wokalek@wokalek.ru>
2024-08-17 12:21:01 +0300

    chore: убрал --build флаг при запуске cron задачи обновления сертификата

--- crontab/certbot-renew.sh
+++ crontab/certbot-renew.sh
@@ -3,5 +3,5 @@
 # Пример crontab:
 # 0 3 * * * bash /root/projects/wokalek-site/crontab/certbot-renew.sh /root/projects/wokalek-site/docker-compose.prod.yml
 
-docker compose -f $1 up --build certbot
+docker compose -f $1 up certbot
 docker restart wokalek-nginx
