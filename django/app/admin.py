9de55053a3e5e78eab82e8c03a576889fad59c2b
Alexandr Wokalek <wokalek@wokalek.ru>
2024-08-17 18:52:08 +0300

    feat: стилизация админки django

--- django/app/admin.py
+++ django/app/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 
 class AppAdminSite(admin.AdminSite):
-    site_header = 'wokalek'
+    site_header = ' '
     site_title = 'Администрирование wokalek.ru'
     index_title = ''
