e31240e1fdfb4708ec60264d73827123bf8ea733
Alexandr Wokalek <wokalek@wokalek.ru>
2025-03-27 23:39:38 +0300

    feat: Добавлен и настроен сервис backrest для бэкапов, убран сервис fonts

--- crontab/certbot-renew.sh
+++ crontab/certbot-renew.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Пример crontab:
-# 0 3 * * * bash /root/projects/wokalek-site/crontab/certbot-renew.sh /root/projects/wokalek-site/docker-compose.prod.yml
+# 0 5 * * * sh /root/projects/wokalek-site/crontab/certbot-renew.sh /root/projects/wokalek-site/docker-compose.prod.yml
 
 docker compose -f $1 up certbot
 docker restart wokalek-nginx
